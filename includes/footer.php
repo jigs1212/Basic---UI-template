30168bb5d4de83039ded29a69b781a222cb5ec45
Akhilkrishna <infoakhilkrishna@gmail.com>
2018-01-02 17:56:23 +0530

    First Init

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+
+    <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js">
+    <script type="text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.min.js">
+    <script type="text/javascript" src="assets/js/main.js">
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ref Home</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
