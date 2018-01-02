75ea1e7104512fa526ded6afd360483c577e9b5e
Akhilkrishna <infoakhilkrishna@gmail.com>
2018-01-02 17:58:51 +0530

    Title changed

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Ref Home</title>
+    <title>Title</title>
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
   </head>
