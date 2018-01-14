09465ce7bfd54075911a939887acfa864a36dcbe
Jigar Lodaya <jigar@bravocode.in>
2018-01-14 17:37:26 +0530

    script bug fixed

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 
-    <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js">
-    <script type="text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.min.js">
-    <script type="text/javascript" src="assets/js/main.js">
+    <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
   </body>
 </html>
